d626ca8ff5d765300d8adf24b61e26679dfd3d3a
Roberto <roberto.p.gomes@hotmail.com>
2022-03-28 20:14:51 -0300

    Second

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata'
 import { ApolloServer } from 'apollo-server'
 import { buildSchema } from 'type-graphql'
 import { context } from './context'
+import { UserResolver } from './user-resolver'
 
 const app = async () => {
-  const schema = await buildSchema({ resolvers: [] })
+  const schema = await buildSchema({ resolvers: [UserResolver] })
 
-  new ApolloServer({ schema, context }).listen({ port: 4000 }),
-    () => console.log('Server is running 🚀')
+  new ApolloServer({ schema, context })
+    .listen({ port: 4000 },
+      () => console.log('Server is running 🚀'))
 }
 
 app()
--- src/user-resolver.ts
+++ src/user-resolver.ts
@@ -1,8 +1,9 @@
 import { Token } from "graphql";
-import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Field, InputType, Mutation, Query, Resolver, ObjectType } from 'type-graphql';
 import { Context, context } from './context';
 import { User } from "./user";
-
+import { hash, compare } from 'bcryptjs'
+import { v4 as uuid } from 'uuid'
 
 @InputType()
 class UserInputData {
@@ -11,6 +12,15 @@ class UserInputData {
 
   @Field()
   password: string
+}
+
+@ObjectType()
+class UserWithToken {
+  @Field()
+  user: User
+
+  @Field()
+  token: string
 }
 
 @Resolver()
@@ -31,8 +41,26 @@ export class UserResolver {
   }
 
   @Mutation((returns) => User)
-  async signUp(@Arg('data') data: UserInputData, @Ctx() ctx: Context): Promise<User>{
-    await ctx.prisma.users.create({data: data})
+  async signUp(@Arg('data') data: UserInputData, @Ctx() ctx: Context): Promise<User> {
+    const hashedPassword = await hash(data.password, 10)
+
+    return ctx.prisma.users.create({ data: { ...data, password: hashedPassword } })
   }
 
+  @Mutation((returns) => UserWithToken)
+  async login(@Arg('data') data: UserInputData, @Ctx() ctx: Context): Promise<User & { token: string } | null> {
+    const user = await ctx.prisma.users.findUnique({ where: { email: data.email } })
+
+    if (!user) return null
+
+    const validation = await compare(data.password, user.password)
+
+    if (!validation) return null
+
+    const tokenCode = uuid()
+
+    const token = await ctx.prisma.tokens.create({ data: { token: tokenCode, user: { connect: { id: user.id } } } })
+
+    return { ...user, token: token.token }
+  }
 }
